755523df8d7ecb717d5c7ecfc440423d5fd42401
ElCono <murphymx100@gmail.com>
2026-05-01 19:27:33 -0300

    cinematicas

---
+++ escenas/menu/cinematica_2.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_continuar_pressed() -> void:
+	get_tree().change_scene_to_file("res://escenas/menu/cinematica_3.tscn")
---
+++ escenas/menu/cinematica_4.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_continuar_pressed() -> void:
+	get_tree().change_scene_to_file("res://escenas/menu/cinematica_5.tscn")
